6f5a69ebc6efd966b796f33d66fb6393e2c6f5f2
simonlotzkar <simonlotzkar@gmail.com>
2025-01-09 12:55:49 -0800

    reverted bento-grid, removed from projects section, reformatted projects in messages.ts

--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -1,21 +1,10 @@
 import React from "react"
-import { BentoGrid, BentoGridItem } from "./ui/bento-grid"
-import { myProjects } from "@/data/messages"
 
+// Projects section.
 const ProjectsSection = () => {
   return (
     <section id="projects">
-      <BentoGrid>
-        {myProjects.map
-        ((item) => (
-          <BentoGridItem
-            id={item.id}
-            key={item.id}
-            title={item.title}
-            description={item.description}
-          />
-        ))}
-      </BentoGrid>
+      
     </section>
   )
 }
--- data/messages.ts
+++ data/messages.ts
@@ -3,8 +3,30 @@ export const myName = "Simon Lotzkar";
 export const myTitle = "Software Developer (Vancouver, BC)";
 
 // Projects Section
-export const myProjects = [
-    {title: "Title1", description: "Description1", id: 1},
-    {title: "Title2", description: "Description2", id: 2},
-    {title: "Title3", description: "Description3", id: 3},
-]
+interface Project {
+    className: string;
+    title: string;
+    description: string;
+    img: string;
+    githubLink?: string;
+    hostedLink?: string;
+    link?: string;
+    linkName?: string;
+}
+export const myProjects: Project[] = [
+    {
+        className: "project-class-1",
+        title: "Project One",
+        description: "Description for project one.",
+        img: "path/to/image1.jpg",
+        githubLink: "https://github.com/user/project-one",
+    },
+    {
+        className: "project-class-2",
+        title: "Project Two",
+        description: "Description for project two.",
+        img: "path/to/image2.jpg",
+        githubLink: "https://github.com/user/project-two",
+        hostedLink: "https://hostedlink2.com",
+    }
+];
