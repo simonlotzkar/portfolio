efdc4df96980a6b43507f4f2bd08f0e551679b30
simonlotzkar <simonlotzkar@gmail.com>
2025-01-10 19:07:03 -0800

    added comments

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,6 +1,24 @@
 import { cn } from '@/lib/utils';
 import React from 'react';
 
+/**
+ * Displays an image next to a title/description/links. Puts them next to eachother
+ * above small view and on top of eachother otherwise.
+ *
+ * @param {Object} props - The properties object.
+ * @param {string} [props.className] - Optional additional class names for the card container.
+ * @param {string} props.title - The title of the project.
+ * @param {string} props.description - The description of the project.
+ * @param {string} props.img - The URL of the project's image.
+ * @param {string} [props.githubLink] - Optional URL to the project's GitHub repository.
+ * @param {string} [props.hostedLink] - Optional URL to the hosted version of the project.
+ * @param {string} [props.link] - Optional additional URL related to the project.
+ * @param {string} [props.linkName] - Optional name for the additional link. Defaults to "Link" if not provided.
+ *
+ * @returns {JSX.Element} The rendered project card component.
+ *
+ * @author SimonLotzkar
+ */
 export const ProjectCard = ({
     className,
     title,
@@ -27,13 +45,22 @@ export const ProjectCard = ({
                 className
             )}
         >
+            {/* Image: Project image. */}
             <img src={img} alt={title} className="shadow-2xl rounded-lg w-4/5 sm:w-2/3 h-auto max-h-96 object-cover" />
+
+            {/* Section: Project Text. */}
             <div className="sm:ml-4 w-4/5 sm:w-1/3">
+
+                {/* Text: Project title. */}
                 <h2 className="truncate text-xl font-bold text-accent">{title}</h2>
-                <p className="whitespace-normal overflow-wrap-break-word">
-                    {description}
-                </p>
+
+                {/* Text: Project description. */}
+                <p className="whitespace-normal overflow-wrap-break-word">{description}</p>
+
+                {/* List: Project links. */}
                 <ul className="ml-4 mt-4 list-disc list-inside">
+
+                    {/* List Item: GitHub link. */}
                     {githubLink && (
                         <li>
                             <a
@@ -46,6 +73,8 @@ export const ProjectCard = ({
                             </a>
                         </li>
                     )}
+
+                    {/* List Item: Hosted link. */}
                     {hostedLink && (
                         <li>
                             <a
@@ -58,6 +87,8 @@ export const ProjectCard = ({
                             </a>
                         </li>
                     )}
+
+                    {/* List Item: Alternative link. */}
                     {link && (
                         <li>
                             <a
--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -2,11 +2,20 @@ import React from "react"
 import ProjectCard from "./ProjectCard"
 import { myProjects } from "@/data/messages"
 
-// Projects section.
+/**
+ * Projects title and project card for each project.
+ *
+ * @component
+ * @returns {JSX.Element} The rendered ProjectsSection component.
+ *
+ * @author SimonLotzkar
+ */
 const ProjectsSection = () => {
   return (
     <section id="projects">
+      {/* Text: Projects title. */}
       <h1 className="text-4xl font-bold py-5 text-center text-secondary">Projects</h1>
+      {/* Section: Project cards. */}
       <div className="max-w-4xl mx-auto space-y-10">
         {myProjects.map(({ className, title, description, img, hostedLink, githubLink, link, linkName }, i) => (
           <ProjectCard
@@ -19,7 +28,7 @@ const ProjectsSection = () => {
             githubLink={githubLink}
             link={link}
             linkName={linkName}
-            />
+          />
         ))}
       </div>
     </section>
