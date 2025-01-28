52cbafd64b27f70a25e577ca91c8be5cd383d6f2
simonlotzkar <simonlotzkar@gmail.com>
2025-01-27 16:38:34 -0800

    skills text added

--- components/SkillsCard.tsx
+++ components/SkillsCard.tsx
@@ -13,16 +13,22 @@ interface SkillsCardProps {
  * @param {string} props.name - The name of the skill
  * @param {string} props.img - The URL of the image representing the skill
  *
- * @returns {JSX.Element} The rendered SkillsCard component.
+ * @returns {React.FC} The rendered SkillsCard component.
  *
  * @author SimonLotzkar
  */
-export const SkillsCard = ({ name, img }: SkillsCardProps) => {
+const SkillsCard: React.FC<SkillsCardProps> = ({
+    name,
+    img
+}) => {
     return (
-        <div className="w-24 h-24 flex items-center justify-center p-4 rounded-lg bg-primary text-center shadow-xl shadow-primary-dark hover:bg-primary-dark z-30 hover:scale-105 transition group">
-            <div className="">
-                {/* Image: Represents the skill with an icon or logo */}
-                <Image width="100" height="100" src={img} alt={name} className="items-center flex justify-center object-contain group-hover:scale-150 z-10" />
+        <div className="w-24 h-24 flex items-center justify-center p-4 rounded-lg bg-primary text-center shadow-xl shadow-primary-dark hover:bg-primary-dark z-30 hover:scale-105 transition group relative">
+            {/* Image: Represents the skill with an icon or logo */}
+            <Image width="100" height="100" src={img} alt={name} className="w-[90%] h-[90%] group-hover:scale-150 z-10" />
+
+            {/* Skill name overlay */}
+            <div className="absolute bottom-0 left-0 right-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity z-20">
+                <span className="text-lg font-bold">{name}</span>
             </div>
         </div>
     );
