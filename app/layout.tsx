983be8be3331be3e4ff5cdfe0ac8b400fe0da2f3
simonlotzkar <simonlotzkar@gmail.com>
2025-02-14 12:18:34 -0800

    updated opengraph / twitter metadata and replaced site thumbnail

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import "./globals.css";
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
 import { siteDescription, siteThumbnailAlt, siteThumbnailPath, siteTitle, siteURL } from "@/data";
-import Head from "next/head";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,26 +18,27 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: siteTitle,
   description: siteDescription,
+  openGraph: {
+    locale: "en_US",
+    title: siteTitle,
+    siteName: siteTitle,
+    description: siteDescription,
+    url: `https://${siteURL}`,
+    type: "website",
+    images: [
+      {
+        url: `https://${siteURL}${siteThumbnailPath}`,
+        width: 1200,
+        height: 630,
+        alt: siteThumbnailAlt,
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
   return (
     <html lang="en">
-      <Head>
-        {/* Opengraph */}
-        <meta property="og:title" key="og:title" content={siteTitle} />
-        <meta property="og:description" key="og:description" content={siteDescription} />
-        <meta property="og:url" key="og:url" content={`https://${siteURL}`} />
-        <meta property="og:type" key="og:type" content="website" />
-        <meta property="og:image" key="og:image" content={siteThumbnailPath} />
-        <meta property="og:image:alt" key="og:image:alt" content={siteThumbnailAlt} />
-        {/* Twitter */}
-        <meta name="twitter:card" key="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" key="twitter:title" content={siteTitle} />
-        <meta name="twitter:description" key="twitter:description" content={siteDescription} />
-        <meta name="twitter:image" key="twitter:image" content={siteThumbnailPath} />
-        <meta name="twitter:image:alt" key="twitter:image:alt" content={siteThumbnailAlt} />
-      </Head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Navbar />
         <main className="w-full relative flex justify-center items-center flex-col">
